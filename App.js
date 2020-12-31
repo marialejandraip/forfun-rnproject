26b29f6daa37679dc91d78046265ee6ce66d8489
marialejandraip <mariaibanez07@gmail.com>
2020-12-30 19:12:13 -0500

    Touchable things to learn, filter event

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 //import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 
 export default function App() {
   const [people, setPeople]= useState([
@@ -18,22 +18,22 @@ export default function App() {
   //Other way using Flat, when is a large array of items is better use it. It can be better for performance.
   //it dont automatically load onto the screen all data, only in the first view and then more will load as the scrolldown 
   //ScrollView will be better when is a more less data and will render all the items from the begining.
+  const pressHandle =(id)=>{
+    console.log(id)
+    setPeople((prevPeople)=>{
+      return prevPeople.filter(person => person.key != id);
+    })
+  }
 
   return (
     <View style={styles.container}>
       <FlatList 
       data={people}
-      renderItem={({person})=>(
-        <Text style={styles.item}>{person.name}</Text>
+      renderItem={({ item })=>(
+        <TouchableOpacity onPress={() => pressHandle(item.key)}>
+          <Text style={styles.item} > {item.name}</Text>
+        </TouchableOpacity>
       )}/>
-  
-    {/*   <ScrollView>
-      { people.map(person => (
-          <View key={person.key} >
-            <Text style={styles.item} >{person.name}</Text>
-          </View>
-        ))}
-      </ScrollView> */}
     </View>
   );
 }
